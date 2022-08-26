e36a89abcadec608d4af603fc27f70d23deda8af
kuma033 <nhayato28@yahoo.co.jp>
2022-08-27 06:39:14 +0900

    change route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <Navbar />
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className='w-11/12 h-5/6 md:w-9/12'>
-        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route exact path='/' element={ <Home />}/>
             <Route path='/about_me' element={ <Self />}/>
@@ -16,7 +15,6 @@ function App() {
             <Route path='/learn_more' element={ <Learning />}/>
             <Route path='/contact' element={ <Contact />}/>
           </Routes>
-        </Router>
       </div>
     </div>
     </>
