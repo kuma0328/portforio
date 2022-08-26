c91cf24111ebc4e3c17130e99e891cccfad5312e
kuma033 <nhayato28@yahoo.co.jp>
2022-08-27 06:22:52 +0900

    Add homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Navbar />
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className='w-11/12 h-5/6 md:w-9/12'>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route exact path='/' element={ <Home />}/>
             <Route path='/about_me' element={ <Self />}/>
