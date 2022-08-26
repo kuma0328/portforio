d4b938ea5e6f0afe7b3c8dd9ee32fae845dfa369
kuma033 <nhayato28@yahoo.co.jp>
2022-08-27 05:04:13 +0900

    change router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import {Home, Self, Skills, Learning, Contact, Navbar} from './components/index';
 
+const ROUTER_BASENAME =
+  process.env.NODE_ENV === 'development' ? '/' : '/repo-name';
+
 function App() {
   return (
     <>
     <Navbar />
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className='w-11/12 h-5/6 md:w-9/12'>
-        <Router>
+        <Router basename={ROUTER_BASENAME}>
           <Routes>
             <Route exact path='/portforio/' element={ <Home />}/>
             <Route path='/portforio/about_me' element={ <Self />}/>
