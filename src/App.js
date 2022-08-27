9794abea29ae4564cfab22a4862dd1fbbc37e89d
kuma033 <nhayato28@yahoo.co.jp>
2022-08-27 15:23:08 +0900

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import {Home, Self, Skills, Learning, Contact, Navbar} from './components/index';
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import {MenuItems} from './MenuItems'
 function Navbar() {
   return (
     <>
-      <nav >
+      <nav>
         <h1 className='bg-black h-20 text-white p-6 text-xl'>
           Portforio
-          </h1>
+        </h1>
         <ul className='Menu list'>
           {MenuItems.map((item, index) => {
             return (
