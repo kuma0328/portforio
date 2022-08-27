eb684a0637e7edbcdc8994a52b0d893b250b9671
kuma033 <nhayato28@yahoo.co.jp>
2022-08-27 16:03:07 +0900

    fix contact images

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,14 @@ import Atcoder_icon from '../images/atcoder.png'
 
 function Contact() {
   return (
-    <div className='flex space-x-12 flex-row justify-center items-center'>
-      <a href='https://twitter.com/42hnakatan'>
+    <div className='flex space-x-12 flex-row justify-center items-center p-20'>
+      <a href='https://twitter.com/42hnakatan' className='w-1/3'>
         <img src={Twitter_icon} alt='Twitter' />
       </a>
-      <a href='https://github.com/kuma0328'>
+      <a href='https://github.com/kuma0328' className='w-1/3'>
         <img src={Github_icon} alt='Github' />
       </a>
-      <a href='https://atcoder.jp/users/kuma033'>
+      <a href='https://atcoder.jp/users/kuma033' className='w-1/3'>
         <img src={Atcoder_icon} alt='Atcoder'/>
       </a>
     </div>
